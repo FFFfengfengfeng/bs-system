8c225bcec0834549613dd89618eebb7da741fdfa
FFFfengfengfeng <1098865109@qq.com>
2017-11-23 17:34:54 +0800

    no message

--- application/index/controller/Data.php
+++ application/index/controller/Data.php
@@ -1,8 +1,15 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017-11-23
+ * Time: 16:19
+ */
+
 namespace app\index\controller;
 use think\Controller;
 
-class Index extends Controller
+class Data extends Controller
 {
     public function index()
     {
