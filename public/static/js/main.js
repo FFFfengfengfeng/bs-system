abacbd1aa730182a0862081b7c9c63a9fcbf7ba4
FFFfengfengfeng <1098865109@qq.com>
2017-11-27 01:41:53 +0800

    添加toast插件

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -1,3 +1,36 @@
 + function (window, $) {
+    'use strict';
+
+    /*
+     * 默认参数
+     */
+    var defaults = {
+        type: 'success',
+        message: '打开了',
+        duration: 1000,
+    };
+
+    /*
+     * 构造函数
+     * @param options
+     */
+    function Toast(options) {
+        this.options = $.extend(defaults, options || {});
+        this.init();
+    }
+    
+    Toast.prototype.init = function () {
+        
+    }
+
+    Toast.prototype.createDOM = function () {
+        var _this   = this,
+            options = _this.options,
+            type    = options.type,
+            message = options.message;
+
+        _this.$toast = $('<div class="toast ' + type + ' clearfix"><p >' + message + '</p><a href="javascript:;" class="fa fa-close"></a></div>');
+    }
+
 
 }(window, jQuery);
