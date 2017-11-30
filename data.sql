0711dcbd077215221d0322eb8d080942f54cc259
FFFfengfengfeng <1098865109@qq.com>
2017-11-30 19:14:52 +0800

    数据导出

--- data.sql
+++ data.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-11-29 18:52:31
+Date: 2017-11-30 19:14:42
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -42,11 +42,12 @@ CREATE TABLE `shop` (
   `shop_address` varchar(255) NOT NULL,
   `charge` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of shop
 -- ----------------------------
+INSERT INTO `shop` VALUES ('1', '永佳丰田', '020-84812345', '广州番禺市桥', '小明');
 
 -- ----------------------------
 -- Table structure for user
@@ -55,13 +56,17 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` int(11) NOT NULL,
-  `logo_name` varchar(255) NOT NULL,
+  `login_name` varchar(255) NOT NULL,
   `user_name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES ('1', '0', 'admin', 'admin', '123456', '13800138000');
+INSERT INTO `user` VALUES ('2', '1', 'abc123', '王小明', '123456', '13800138001');
+INSERT INTO `user` VALUES ('3', '2', 'abc456', '张小红', '123456', '13800138002');
+INSERT INTO `user` VALUES ('13', '1', '11', '22', '22', '22');
