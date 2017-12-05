59c121b4d043c8cf2611ae15525300d8a03a184f
FFFfengfengfeng <1098865109@qq.com>
2017-12-05 10:33:11 +0800

    添加申请

---
+++ application/index/controller/apply.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017-12-05
+ * Time: 9:34
+ */
+
+namespace app\index\controller;
+
+
+class apply extends Base
+{
+    public function index()
+    {
+        
+    }
+}
